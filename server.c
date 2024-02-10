9f577c9f1a8dd8acfff17a37e57d7830a28f3755
oussama seffai <ousf.dev@gmail.com>
2024-02-10 22:01:33 +0100

    fix not persistent socket

--- server.c
+++ server.c
@@ -1,9 +1,9 @@
-#include <stdio.h>      // Standard input/output definitions
-#include <stdlib.h>     // Standard library for memory allocation, process control, etc.
-#include <string.h>     // String handling functions
-#include <sys/socket.h> // Main sockets header
-#include <netinet/in.h> // Internet address family
-#include <unistd.h>     // POSIX operating system API
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
 
 int main()
 {
@@ -16,13 +16,32 @@ int main()
   address.sin_port = htons(8080);       // Port number
 
   bind(sockfd, (struct sockaddr *)&address, sizeof(address));
-
   listen(sockfd, 3); // The second argument is the number of pending connections allowed
 
-  int new_socket = accept(sockfd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
+  while (1)
+  { // Run an infinite loop to accept connections continuously
+    int new_socket = accept(sockfd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
+    if (new_socket < 0)
+    {
+      perror("accept");
+      exit(EXIT_FAILURE);
+    }
 
-  char *hello = "HTTP/1.1 200 OK\nContent-Type: text/plain\n\nHello, world!";
-  write(new_socket, hello, strlen(hello));
+    // Read the request from client
+    char buffer[1024] = {0};
+    read(new_socket, buffer, 1024);
+    printf("Request: %s\n", buffer); // Print the client's request to standard output
 
+    // Send the HTTP response
+    char *hello = "HTTP/1.1 200 OK\nContent-Type: text/plain\n\nHello, world!";
+    write(new_socket, hello, strlen(hello));
+
+    // Close the socket for the current connection
+    close(new_socket);
+  }
+
+  // Close the listening socket (this line will never be reached in this example)
   close(sockfd);
+
+  return 0;
 }
