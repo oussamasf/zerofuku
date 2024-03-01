5a71eabd2220a98831b36038940053a8f769fd88
oussama seffai <ousf.dev@gmail.com>
2024-03-01 22:40:23 +0100

    write another server

--- server.c
+++ server.c
@@ -1,47 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <unistd.h>
+
+void handle_request(int client_socket)
+{
+  char request[1024];
+  int bytes_received = recv(client_socket, request, 1024, 0);
+
+  if (bytes_received > 0)
+  {
+
+    if (strncmp(request, "GET /", 4) == 0)
+    {
+      char response[] = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello from the HTTP server!\n";
+      send(client_socket, response, strlen(response), 0);
+    }
+    else
+    {
+      char error_response[] = "HTTP/1.1 405 Method Not Allowed\r\n\r\n";
+      send(client_socket, error_response, strlen(error_response), 0);
+    }
+  }
+
+  close(client_socket);
+}
 
 int main()
 {
-  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  struct sockaddr_in address;
-  int addrlen = sizeof(address);
+  int server_socket, client_socket;
+  struct sockaddr_in server_address;
 
-  address.sin_family = AF_INET;
-  address.sin_addr.s_addr = INADDR_ANY; // Listen on any IP address
-  address.sin_port = htons(8080);       // Port number
-
-  bind(sockfd, (struct sockaddr *)&address, sizeof(address));
-  listen(sockfd, 3); // The second argument is the number of pending connections allowed
-
-  while (1)
-  { // Run an infinite loop to accept connections continuously
-    int new_socket = accept(sockfd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
-    if (new_socket < 0)
-    {
-      perror("accept");
-      exit(EXIT_FAILURE);
-    }
-
-    // Read the request from client
-    char buffer[1024] = {0};
-    read(new_socket, buffer, 1024);
-    printf("Request: %s\n", buffer); // Print the client's request to standard output
-
-    // Send the HTTP response
-    char *hello = "HTTP/1.1 200 OK\nContent-Type: text/plain\n\nHello, world!";
-    write(new_socket, hello, strlen(hello));
-
-    // Close the socket for the current connection
-    close(new_socket);
+  // Create a socket
+  server_socket = socket(AF_INET, SOCK_STREAM, 0);
+  if (server_socket == -1)
+  {
+    perror("Failed to create socket");
+    exit(1);
   }
 
-  // Close the listening socket (this line will never be reached in this example)
-  close(sockfd);
+  // Configure server address
+  server_address.sin_family = AF_INET;
+  server_address.sin_addr.s_addr = INADDR_ANY;
+  server_address.sin_port = htons(8080); // Listen on port 8080
 
+  // Bind the socket to the address
+  if (bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address)) == -1)
+  {
+    perror("Failed to bind socket");
+    exit(1);
+  }
+
+  if (listen(server_socket, 5) == -1)
+  {
+    perror("Failed to listen on socket");
+    exit(1);
+  }
+
+  printf("Server listening on port 8080...\n");
+
+  while (1)
+  {
+    client_socket = accept(server_socket, NULL, NULL);
+    if (client_socket == -1)
+    {
+      perror("Failed to accept connection");
+      continue;
+    }
+
+    handle_request(client_socket);
+  }
+
+  close(server_socket); // Close the server socket before exiting
   return 0;
 }
